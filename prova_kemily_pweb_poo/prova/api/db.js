bc030343ee49dc89daf7daaddfeb9b02169db1b9
KemilyMaria <131410450+KemilyMaria@users.noreply.github.com>
2023-06-10 15:41:22 -0300

    Update db.js

--- prova_kemily_pweb_poo/prova/api/db.js
+++ prova_kemily_pweb_poo/prova/api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123456789",
+    password: "12345678",
     database: "crud"
 })
